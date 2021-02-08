03f0e4862fbdb85c96c84f7be345f1cf98cb4bbf
larlarlar <larisa.gurina.s@gmail.com>
2021-02-08 22:52:22 +0300

    fall once again

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -6,14 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class StatisticsServiceTest {
 
   @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
-
+  void shouldFindMax() {
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    StatisticsService service = new StatisticsService();
     long expected = 12;
-
     long actual = service.findMax(incomesInBillions);
-
     assertEquals(expected, actual);
   }
 }
+
