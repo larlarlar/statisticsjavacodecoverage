6b32bddd1f9e137d6a953e07a4fa33632b00421e
larlarlar <larisa.gurina.s@gmail.com>
2021-02-08 22:59:02 +0300

    raise once again

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -13,5 +13,14 @@ class StatisticsServiceTest {
     long actual = service.findMax(incomesInBillions);
     assertEquals(expected, actual);
   }
+
+  @Test
+  void shouldFindMax2() {
+    long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    StatisticsService service = new StatisticsService();
+    long expected = 12;
+    long actual = service.findMax(incomesInBillions);
+    assertEquals(expected, actual);
+  }
 }
 
